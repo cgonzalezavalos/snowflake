1907a393019c3e16e2f28b3e839944a8879c447a
cgonzalezavalos <cagonzalez.avalos@gmail.coml>
2023-10-25 10:51:48 -0300

    mas sidebar

--- app_snow.py
+++ app_snow.py
@@ -6,7 +6,10 @@ from datetime import date
 
 sidebar =st.sidebar
 
-whith sidebar:
+with sidebar:
         account=st.text_input("account")
         username=st.text_input("username")
         password=st.text_input("password",type="password")
+        role=st.text_input("role")
+        wh=st.text_input("Warehouse")
+        db=st.text_input("database")
