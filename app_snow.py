c4ea36087bfa00464d070101b166bb58da7d6aac
cgonzalezavalos <cagonzalez.avalos@gmail.coml>
2023-10-25 10:48:57 -0300

    sidebar

--- app_snow.py
+++ app_snow.py
@@ -2,3 +2,11 @@ import pandas as pd
 import streamlit as st
 import snowflake.connector as sfc
 from datetime import date
+
+
+sidebar =st.sidebar
+
+whith sidebar:
+        account=st.text_input("account")
+        username=st.text_input("username")
+        password=st.text_input("password",type="password")
